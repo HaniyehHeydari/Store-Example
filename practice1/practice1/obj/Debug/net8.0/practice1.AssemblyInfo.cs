112d2f4e897acf0769090e96da077d0dd8e7b143
haniyeh <heydarihaniyeh51@gmail.com>
2024-03-01 02:41:35 +0330

    add Header

--- practice1/practice1/obj/Debug/net8.0/practice1.AssemblyInfo.cs
+++ practice1/practice1/obj/Debug/net8.0/practice1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("practice1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+359b87848166907d7a86eda838c68baf983ff373")]
 [assembly: System.Reflection.AssemblyProductAttribute("practice1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("practice1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
